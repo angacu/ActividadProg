e6430d50402628d82c79a1fc9053e1f0621846d8
Andreu Garcia Cueva <angacu@floridauniversitaria.es>
2016-10-05 11:46:51 +0200

    pInvadersF1-master

---
+++ pInvadersF1-master/lib/ia.php
@@ -0,0 +1,24 @@
+<?php 
+
+class ia
+//ATRIBUTOS
+{
+public $columna=1 ;
+public $fila=4 ;
+
+//METODOS
+public function getFila(){
+return $this->fila ;
+
+
+}
+
+public function getColumna(){
+return $this->columna ;
+
+}
+}
+
+
+
+?> 
